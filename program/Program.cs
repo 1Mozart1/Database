10a57b5df5dbc8e7a6fe0387126d26068e6b1947
1Mozart1 <andreivm2015@yandex.ru>
2023-03-12 14:09:24 +0500

    program

---
+++ program/Program.cs
@@ -0,0 +1,36 @@
+﻿string[] InputArray() 
+{
+    System.Console.Write("Введите значения через пробел: ");
+    return Console.ReadLine()!.Split(" ");
+}
+
+int CountLess(string[] input, int n) 
+{
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] OutputArray(string[] input, int n) 
+{
+    string[] output = new string[CountLess(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+string[] array = InputArray();
+string[] result = OutputArray(array, 3);
+System.Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
